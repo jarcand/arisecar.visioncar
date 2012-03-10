cfb2cc63badbdec1dd1a8238a22d5f17b2a30619
Marc-Andre <marc-andrepc@gmail.com>
2012-03-10 11:12:46 -0500

    Adding real things, In a separate folder currently but will merge it later on with the simulation.

---
+++ src/real/realWorld/World.java
@@ -0,0 +1,44 @@
+package real.realWorld;
+
+import real.pointMap.FogMap;
+import real.pointMap.PointMap;
+import real.pointMap.VisionCar;
+
+/**
+ * A practical design I came up with while coding game. It's very useful for 
+ * developing quick changing code like prototype, simulation, game, etc.
+ * 
+ * Basically, this class create a link between every other class. So at any time,
+ * one class can access another class in just one step. Every object, in the world
+ * should have a reference to the World. They might not need it at the time you
+ * create it but they might need it at a later point.
+ * 
+ * The other class still maintain their object encapsulation so they decide what 
+ * they want to show to the rest of the world and what to hide.
+ * 
+ * Anyway, that is just something I use for fixing some problem of creating a big
+ * network of interdepent class that is hard to deal with. Such network is not scalable
+ * and not refactorable. At least in my experience.
+ * 
+ * On a side note, this design work well as long as you are the only one working on
+ * your code. If other people are working in a project with you, you should probably
+ * hide away the world class under some interface so they can't do a mess with using
+ * some part of your code that should only be accessible to the thing you define 
+ * have an access to the world yourself.
+ * 
+ * @author Gudradain
+ *
+ */
+public class World {
+	
+	public final PointMap pointMap;
+	public final VisionCar visionCar;
+	public final FogMap fogMap;
+	
+	public World(){
+		pointMap = new PointMap(this);
+		visionCar = new VisionCar(this);
+		fogMap = new FogMap(this);
+	}
+
+}
