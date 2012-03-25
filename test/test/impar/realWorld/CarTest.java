6dfe358ce1a0eaacc765a6308990fd2c5bbaf428
Marc-Andre <marc-andrepc@gmail.com>
2012-03-25 12:22:16 -0400

    It want to commit that

---
+++ test/test/impar/realWorld/CarTest.java
@@ -0,0 +1,38 @@
+package test.impar.realWorld;
+
+
+import static org.junit.Assert.fail;
+
+import java.awt.Component;
+import java.awt.event.KeyEvent;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CarTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testMovementForward() {
+		fail("Not yet implemented");
+	}
+
+}
